804309791fe402f18010857e6089e31cf0860168
sufiyansajid <sufiyansajid0000@gmail.com>
2025-05-03 19:05:20 +0500

    add project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,14 +14,13 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Online Food Ordering System</h3>
+              <h3 className="text-xl font-bold mb-2"> Music Player Web App</h3>
               <p className="text-gray-400 mb-4">
-                An Online Food Ordering System
-                with a normalized SQL database, managing users, orders,
-                restaurants, and payments.
+                Music Player Web App is a Html, CSS and JS based music player 
+                which implement play, pause and volume control functionalities.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["SQL"].map((tech, key) => (
+                {["HTML,CSS,JS"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -35,7 +34,7 @@ export const Projects = () => {
 
               <div className="flex justify-between items-center">
                 <a
-                  href="https://github.com/sufiyansajid/Online-Food-Ordering-System.git"
+                  href="https://github.com/sufiyansajid/Music-Player.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
